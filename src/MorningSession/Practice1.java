fe303753037cdef27f9855aa21df7d5d0e229284
ali.bilen <ali.bilen@outlook.com>
2024-09-10 19:48:21 +0200

    10/09/2024

---
+++ src/MorningSession/Practice1.java
@@ -0,0 +1,10 @@
+package MorningSession;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+
+        // just created not added to staging area
+        // Added to staging area
+    }
+}
